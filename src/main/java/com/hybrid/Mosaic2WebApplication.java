74e506752998377a7aedf18770086df1c2ab5ebd
java <java>
2016-07-28 17:38:33 +0900

    first commit

---
+++ src/main/java/com/hybrid/Mosaic2WebApplication.java
@@ -0,0 +1,12 @@
+package com.hybrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mosaic2WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mosaic2WebApplication.class, args);
+	}
+}
